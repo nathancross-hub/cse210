73b78722cc1790727d8b2e84710391d8356607a9
Nathan Cross <crosskidnate@gmail.com>
2026-09-16 14:30:31 -0600

    Prep 3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        Console.WriteLine(number);
+        int guess = -1;
+        while (guess != number) {
+        Console.WriteLine("Please guess the number: ");
+        string gues = Console.ReadLine();
+        guess = int.Parse(gues);
+        if (guess < number)
+        {
+            Console.WriteLine("Higher!");
+        }
+        else if (guess > number)
+        {
+            Console.WriteLine("Lower!");
+        }
+        }
+        Console.WriteLine("That was the number, congrats!");
     }
 }
