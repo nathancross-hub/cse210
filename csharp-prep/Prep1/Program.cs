e36b84e109cb21139d9ab93936e7594100857c11
Nathan Cross <crosskidnate@gmail.com>
2026-09-16 14:02:50 -0600

    grade calculator / name activities complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+        Console.WriteLine(" ");
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}. ");
     }
 }
+
+
+/*
+What is your first name? Scott
+What is your last name? Burton
+
+Your name is Burton, Scott Burton.
+*/
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please Enter your grade percentage: ");
+        string per = Console.ReadLine();
+        float perc = float.Parse(per);
+        string grade = "notDefined";
+        if (perc >= 90)
+        {
+            grade = "A";
+        }
+        else if (perc >= 80)
+        {
+            grade = "B";
+        }
+        else if (perc >= 70)
+        {
+            grade = "C";
+        }
+        else if (perc >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your grade is {grade}.");
     }
 }
