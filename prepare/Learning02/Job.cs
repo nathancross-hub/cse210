c2b5202c6b7db8dcabbb446710e227e0d5531d1e
Nathan Cross <crosskidnate@gmail.com>
2026-09-22 14:54:34 -0600

    Learning02 complete

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,29 @@
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+        public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
